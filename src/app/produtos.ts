b565ab34902c56504e1c4761ba7d21923f929ed7
cloudsystems22 <cloudsystems22@gmail.com>
2020-10-25 14:23:36 -0300

    Produtos

---
+++ src/app/produtos.ts
@@ -0,0 +1,17 @@
+export const produtos = [
+    {
+        nome:"Sansung Galax S",
+        valor: 2300.12,
+        detalhes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis mi congue, blandit tellus eu, fermentum ex. Mauris quis felis sed nibh mattis tristique non at felis."
+    },
+    {
+        nome:"Sansung Galax x",
+        valor: 2240.01,
+        detalhes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis mi congue, blandit tellus eu, fermentum ex. Mauris quis felis sed nibh mattis tristique non at felis."
+    },
+    {
+        nome:"iPhone X",
+        valor: 3560.92,
+        detalhes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis mi congue, blandit tellus eu, fermentum ex. Mauris quis felis sed nibh mattis tristique non at felis."
+    }
+]
